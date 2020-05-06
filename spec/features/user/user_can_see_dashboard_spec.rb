9cd64dda42eb9efe5c95009aa5763dbcebbc887f
Alex Lynch <mlynch5187@gmail.com>
2020-05-06 13:19:01 -0600

    Add if statement to user show page regarding showing Github section

--- spec/features/user/user_can_see_dashboard_spec.rb
+++ spec/features/user/user_can_see_dashboard_spec.rb
@@ -22,4 +22,25 @@ describe 'User' do
 
     expect(page).to have_content("Github")
   end
+
+  it 'cant see a Github section on the dashboard if user is tokenless' do
+    user = User.create(email: "joe@example.com",
+                      first_name: "Joe",
+                      last_name: "Biden",
+                      password: "password",
+                      role: "default")
+
+    visit '/'
+
+    click_on "Sign In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'session[email]', with: user.email
+    fill_in 'session[password]', with: user.password
+
+    click_on 'Log In'
+
+    expect(page).to_not have_content("Github")
+  end
 end
