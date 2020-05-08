265ae25543cac288c85548b3456c5970316cfa96
Alex Lynch <mlynch5187@gmail.com>
2020-05-07 21:53:59 -0600

    Fix unaligned ends in search_result.rb

--- app/poros/search_result.rb
+++ app/poros/search_result.rb
@@ -1,42 +1,42 @@
 class SearchResult
 
-    def repos(token)
-      conn = Faraday.new('https://api.github.com') do |faraday|
-        faraday.headers['Content-Type'] = 'application/json'
-        faraday.headers['Authorization'] = "token #{token}"
-      end
-
-      repos = conn.get('/user/repos')
-      json = JSON.parse(repos.body, symbolize_names: true)
-      @repos = json.map do |github_repos|
-        Repo.new(github_repos)
-      end
+  def repos(token)
+    conn = Faraday.new('https://api.github.com') do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['Authorization'] = "token #{token}"
     end
 
+    repos = conn.get('/user/repos')
+    json = JSON.parse(repos.body, symbolize_names: true)
+    @repos = json.map do |github_repos|
+      Repo.new(github_repos)
+    end
+  end
 
-    def followers(token)
-      conn = Faraday.new('https://api.github.com') do |faraday|
-        faraday.headers['Content-Type'] = 'application/json'
-        faraday.headers['Authorization'] = "token #{token}"
-      end
 
-      follower_list = conn.get('/user/followers')
-      json = JSON.parse(follower_list.body, symbolize_names: true)
-      @followers = json.map do |github_repos|
-        Follow.new(follower_list)
-      end
+  def followers(token)
+    conn = Faraday.new('https://api.github.com') do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['Authorization'] = "token #{token}"
     end
 
-    def following(token)
-      conn = Faraday.new('https://api.github.com') do |faraday|
-        faraday.headers['Content-Type'] = 'application/json'
-        faraday.headers['Authorization'] = "token #{token}"
-      end
+    follower_list = conn.get('/user/followers')
+    json = JSON.parse(follower_list.body, symbolize_names: true)
+    @followers = json.map do |github_repos|
+      Follow.new(follower_list)
+    end
+  end
 
-      following_list = conn.get('user/following')
-      json = JSON.parse(following_list.body, symbolize_names: true)
-      @following = json.map do |github_repos|
-        Follow.new(following_list)
+  def following(token)
+    conn = Faraday.new('https://api.github.com') do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['Authorization'] = "token #{token}"
+    end
+
+    following_list = conn.get('user/following')
+    json = JSON.parse(following_list.body, symbolize_names: true)
+    @following = json.map do |github_repos|
+      Follow.new(following_list)
     end
   end
 end
