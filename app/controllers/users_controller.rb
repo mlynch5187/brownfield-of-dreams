1ceb81a799d0e8c8374edb8c7ec29ef7447426d1
Alex Lynch <mlynch5187@gmail.com>
2020-05-12 14:48:57 -0600

    Update visitor registration spec to test for flash messages, add flash to user controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = 'Logged in as Jim Bob'
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
