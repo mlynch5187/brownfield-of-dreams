e01670cffeac3bc93721e7db84150a03271b8084
“Krista <stadler.krista@gmail..com>
2020-05-11 22:14:13 -0600

    Mocked testing for user can see dashboard spec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     last_name { Faker::Artist.name }
     password { Faker::Color.color_name }
     role { :default }
-    token { ENV["GITHUB_TOKEN"]}
   end
 
   factory :admin, parent: :user do
--- spec/features/user/user_can_log_in_and_out_spec.rb
+++ spec/features/user/user_can_log_in_and_out_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can sign in' do
+  it 'user can sign in and out' do
     user = create(:user)
 
     visit '/'
@@ -19,27 +19,12 @@ describe 'User' do
     expect(page).to have_content(user.email)
     expect(page).to have_content(user.first_name)
     expect(page).to have_content(user.last_name)
-  end
-
-  it 'can log out', :js do
-    user = create(:user)
-
-    visit login_path
-
-    fill_in'session[email]', with: user.email
-    fill_in'session[password]', with: user.password
-
-    click_on 'Log In'
-
-    click_on 'Profile'
-
-    expect(current_path).to eq(dashboard_path)
 
     click_on 'Log Out'
 
     expect(current_path).to eq(root_path)
     expect(page).to_not have_content(user.first_name)
-    expect(page).to have_content('SIGN IN')
+    expect(page).to have_content('Sign In')
   end
 
   it 'is shown an error when incorrect info is entered' do
