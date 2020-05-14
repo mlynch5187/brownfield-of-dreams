46bb364d68abf87d58dd44cef78c7091bf815ce8
Alex Lynch <mlynch5187@gmail.com>
2020-05-13 20:39:08 -0600

    Update production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,12 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'localhost',
+    :port           => '1025'
+  }
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
