81bfc4fab58a9ff833e8c24640fe130ef402cc2c
“Krista <stadler.krista@gmail..com>
2020-05-13 21:00:42 -0600

    Cleaned up code

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,13 +5,4 @@ class Follow
     @login = attributes[:login]
     @html_url = attributes[:html_url]
   end
-
-  def has_account?
-    # call API https://api.github.com/users/kristastadler
-    # if User.find(API returned email address of follower/following)
-    #   true
-    # else
-    #   false
-  # end
-  end 
 end
