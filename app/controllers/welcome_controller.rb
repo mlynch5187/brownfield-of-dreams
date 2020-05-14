b9dec8d8113937a9852c9c4de28a2ecc0cab84d6
“Krista <stadler.krista@gmail..com>
2020-05-13 20:37:41 -0600

    Tested registration mailer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag])
-                           .paginate(page: params[:page], per_page: 5)
-    else
+    # if params[:tag]
+    #   @tutorials = Tutorial.tagged_with(params[:tag])
+    #                        .paginate(page: params[:page], per_page: 5)
+    # else
       @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    end
+    # end
   end
 end
